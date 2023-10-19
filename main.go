54a056cb0f273eb73f050ccb07867dda6d665245
marco0086 <85660544+marco0086@users.noreply.github.com>
2023-10-18 22:14:42 -0300

    add first simple api rest

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
 	println("hola mundo go")
+	// routes
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
+
+	// start the server
+	http.ListenAndServe(":3000", nil)
 }
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello go"))
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	for i := 1000; i < 1100; i++ {
+		fmt.Printf("%d \t %b \t %x \n", i, i, i)
+		w.Write([]byte("contact page"))
+	}
+
+}
+
+// go run main.go
+// go build main.go
+// ./main
